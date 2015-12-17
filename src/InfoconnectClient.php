9d7a7911749ec208da489301a85f4f8215996df7
Karl Hughes <khughes.me@gmail.com>
2015-12-17 17:41:21 -0500

    Setting up skeleton

---
+++ src/InfoconnectClient.php
@@ -0,0 +1,6 @@
+<?php namespace JobBrander\Clients;
+
+class InfoconnectClient
+{
+    
+}
---
+++ tests/src/DiceTest.php
@@ -0,0 +1,9 @@
+<?php namespace JobBrander\Clients\Test;
+
+use JobBrander\Clients\InfoconnectClient;
+use Mockery as m;
+
+class DiceTest extends \PHPUnit_Framework_TestCase
+{
+    //
+}
